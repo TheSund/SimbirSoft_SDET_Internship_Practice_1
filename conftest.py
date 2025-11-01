946bfff2065c6fcdd36c3e0ca7af4c06485a630e
TheSund <1he5und@gmail.com>
2025-11-01 19:44:12 +0300

    Small adjustment to CI workflow

--- conftest.py
+++ conftest.py
@@ -8,6 +8,8 @@ from data.constants import BASE_URL
 def driver():
     options = webdriver.EdgeOptions()
     options.add_argument('--enable-javascript')
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
     _driver = webdriver.Edge(options=options)
     _driver.get(BASE_URL)
     yield _driver
